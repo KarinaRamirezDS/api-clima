5a03ec925ad2569ad17a05836f5bda3311b8e6b0
KarinaRamirezDS <karinaramirezds@gmail.com>
2021-11-15 22:04:11 -0600

    pruebas

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 //Views
 import Home from "./Pages/Home/Home";
 import WeatherInformation from "./Pages/WeatherInformation/WeatherInformation";
-import Error from "./Components/Custom/Error/Error";
+
 //Layout 
 //import MainLayout from "./Components/Layouts/MainLayout";
 function App() {
@@ -24,10 +24,7 @@ function App() {
           <WeatherInformation />
         </Route>
 
-        <Route path="*">
-        <Error />
-        </Route>
-        
+      
       </Switch>
     </BrowserRouter>
   );
